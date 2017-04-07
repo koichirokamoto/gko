73aa54cca3bad70f83ca2a434aac837fe068ba03
Koichiro Okamoto <koichiro.okamoto@topgate.co.jp>
2017-04-07 15:52:00 +0900

    cloud datastore client

---
+++ cloud_datastore.go
@@ -0,0 +1,69 @@
+package gko
+
+import (
+	"golang.org/x/net/context"
+
+	"cloud.google.com/go/datastore"
+	"google.golang.org/api/option"
+)
+
+var (
+	_ CloudDatastoreFactory = (*cloudDatastoreFactoryImpl)(nil)
+	_ CloudDatastore        = (*cloudDatastoreClient)(nil)
+)
+
+var cloudDatastoreFactory CloudDatastoreFactory
+
+// GetCloudDatastoreFactory return cloudDatastore factory.
+func GetCloudDatastoreFactory(factory CloudDatastoreFactory) CloudDatastoreFactory {
+	if cloudDatastoreFactory == nil {
+		cloudDatastoreFactory = &cloudDatastoreFactoryImpl{}
+	}
+	_, ok := cloudDatastoreFactory.(*cloudDatastoreFactoryImpl)
+	if !ok {
+		cloudDatastoreFactory = &cloudDatastoreFactoryImpl{}
+	}
+	return cloudDatastoreFactory
+}
+
+// CloudDatastoreFactory is cloudDatastore factory interface.
+type CloudDatastoreFactory interface {
+	New(context.Context) (CloudDatastore, error)
+}
+
+// gaeCloudDatastoreFactoryImpl is implementation of cloudDatastore factory.
+type cloudDatastoreFactoryImpl struct{}
+
+// New return cloudDatastore client.
+func (b *cloudDatastoreFactoryImpl) New(ctx context.Context) (CloudDatastore, error) {
+	return newCloudDatastoreClient(ctx)
+}
+
+// CloudDatastore is cloudDatastore interface along with reader and writer.
+type CloudDatastore interface {
+}
+
+// cloudDatastoreClient is cloud datastore client
+type cloudDatastoreClient struct {
+	ctx    context.Context
+	client *datastore.Client
+}
+
+func newCloudDatastoreClient(ctx context.Context) (*cloudDatastoreClient, error) {
+	t, projectID, err := getDefaultTokenSource(ctx, datastore.ScopeDatastore)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := datastore.NewClient(ctx, projectID, option.WithTokenSource(t))
+	if err != nil {
+		return nil, err
+	}
+
+	return &cloudDatastoreClient{ctx, client}, nil
+}
+
+// GetAll get all entities from cloud datastore.
+func (c *cloudDatastoreClient) GetAll(q *datastore.Query, dst []interface{}) ([]*datastore.Key, error) {
+	return c.client.GetAll(c.ctx, q, dst)
+}
