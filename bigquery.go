86dcf57e1f844153a5050856ecb065513b71de15
Koichiro Okamoto <koichiro.okamoto@topgate.co.jp>
2017-05-01 16:04:03 +0900

    update bigquery client to add some operation

--- bigquery.go
+++ bigquery.go
@@ -1,16 +1,26 @@
 package gko
 
 import (
+	"errors"
+
 	"cloud.google.com/go/bigquery"
 
 	"golang.org/x/net/context"
 
+	"fmt"
+
+	"google.golang.org/api/googleapi"
 	"google.golang.org/api/option"
 )
 
 var (
 	_ BigQueryFactory = (*bigQueryFactoryImpl)(nil)
 	_ BigQuery        = (*bigQueryClient)(nil)
+)
+
+var (
+	errTableAlreadyExist   = errors.New("table is already exist")
+	errDatasetAlreadyExist = errors.New("dataset is already exist")
 )
 
 var bigqueryFactory BigQueryFactory
@@ -50,7 +60,7 @@ type BigQueryReader interface {
 
 // BigQueryWriter is bigquery writer interface.
 type BigQueryWriter interface {
-	CreateTable(dataset, table string) error
+	CreateTable(dataset, table string, useStdSQL bool) error
 	DeleteTable(dataset, table string) error
 	UploadRow(dataset, table, suffix string, src interface{}) error
 }
@@ -96,27 +106,86 @@ func (b *bigQueryClient) GetQueryResult(job *bigquery.Job) (*bigquery.RowIterato
 	return job.Read(b.ctx)
 }
 
+// CreateDataset creates new bigquery dataset.
+//
+// If dataset is already exist, return error.
+func (b *bigQueryClient) CreateDataset(dataset string) error {
+	if err := b.checkDatasetExist(dataset); err != nil {
+		return err
+	}
+	return b.client.Dataset(dataset).Create(b.ctx)
+}
+
+func (b *bigQueryClient) DeleteDataset(dataset string) error {
+	if err := b.checkDatasetExist(dataset); err != errDatasetAlreadyExist {
+		if err == nil {
+			return fmt.Errorf("%s is not already exist", dataset)
+		}
+		return err
+	}
+	return b.client.Dataset(dataset).Delete(b.ctx)
+}
+
 // CreateTable create table in dataset bigquery client have.
 //
 // This method always create table with standard sql option.
-func (b *bigQueryClient) CreateTable(dataset, table string) error {
-	return b.client.Dataset(dataset).Table(table).Create(b.ctx, bigquery.UseStandardSQL())
+// If table is already exist in dataset, return error.
+func (b *bigQueryClient) CreateTable(dataset, table string, useStdSQL bool) error {
+	if err := b.checkTableExist(dataset, table); err != nil {
+		return err
+	}
+	var opts []bigquery.CreateTableOption
+	if useStdSQL {
+		opts = append(opts, bigquery.UseStandardSQL())
+	}
+	return b.client.Dataset(dataset).Table(table).Create(b.ctx, opts...)
 }
 
 // DeleteTable delete table in dataset bigquery client have.
 func (b *bigQueryClient) DeleteTable(dataset, table string) error {
+	if err := b.checkTableExist(dataset, table); err != errTableAlreadyExist {
+		if err == nil {
+			return fmt.Errorf("%s:%s is not already exist", dataset, table)
+		}
+		return err
+	}
 	return b.client.Dataset(dataset).Table(table).Delete(b.ctx)
 }
 
 // UploadRow upload one or more row.
 func (b *bigQueryClient) UploadRow(dataset, table, suffix string, src interface{}) error {
 	t := b.client.Dataset(dataset).Table(table)
-	// check src is valid
-	if _, err := bigquery.InferSchema(src); err != nil {
-		return err
-	}
-
 	upl := t.Uploader()
 	upl.TableTemplateSuffix = suffix
 	return upl.Put(b.ctx, src)
+}
+
+// checkTableExist checks bigquery table is already exist.
+//
+// If table is already exist, return error.
+func (b *bigQueryClient) checkTableExist(dataset, table string) error {
+	_, err := b.client.Dataset(dataset).Table(table).Metadata(b.ctx)
+	if err != nil {
+		// if status code is not 404, return error because of exist
+		if gapierr, ok := err.(*googleapi.Error); ok && gapierr.Code == 404 {
+			return nil
+		}
+		return err
+	}
+	return errTableAlreadyExist
+}
+
+// checkDatasetExist checks bigquery dataset is already exist.
+//
+// If dataset is already exist, return error.
+func (b *bigQueryClient) checkDatasetExist(dataset string) error {
+	_, err := b.client.Dataset(dataset).Metadata(b.ctx)
+	if err != nil {
+		// if status code is not 404, return error because of exist
+		if gapierr, ok := err.(*googleapi.Error); ok && gapierr.Code == 404 {
+			return nil
+		}
+		return err
+	}
+	return errDatasetAlreadyExist
 }
