9a86ada1530a51d2638e053ba4b497c472c0a3b4
Koichiro Okamoto <koichiro.okamoto@topgate.co.jp>
2017-05-11 16:01:44 +0900

    delete custom type of storage bucket and replace it to string

--- cloud_storage.go
+++ cloud_storage.go
@@ -1,7 +1,6 @@
 package gko
 
 import (
-	"fmt"
 	"io"
 	"io/ioutil"
 
@@ -11,7 +10,6 @@ import (
 
 	"golang.org/x/oauth2"
 	"google.golang.org/api/option"
-	"google.golang.org/appengine"
 )
 
 var (
@@ -20,9 +18,6 @@ var (
 )
 
 var cloudStorageFactory CloudStorageFactory
-
-// Bucket is cloud storage Bucket.
-type Bucket string
 
 // CloudStorageFactory is cloud storage factory interface.
 type CloudStorageFactory interface {
@@ -40,15 +35,15 @@ type CloudStorage interface {
 
 // CloudStorageReader is cloud storage reader interface.
 type CloudStorageReader interface {
-	DownloadFile(Bucket, string) ([]byte, error)
+	DownloadFile(string, string) ([]byte, error)
 }
 
 // CloudStorageWriter is cloud storage writer interface.
 type CloudStorageWriter interface {
-	CreateFile(Bucket, string, io.Reader) error
-	DeleteFile(Bucket, string) error
-	Copy(b Bucket, src, dst string) error
-	Move(b Bucket, src, dst string) error
+	CreateFile(string, string, io.Reader) error
+	DeleteFile(string, string) error
+	Copy(srcBucket string, dstBucket string, srcFile string, dstFile string) error
+	Move(srcBucket string, dstBucket string, srcFile string, dstFile string) error
 }
 
 // cloudStorageClient is cloud storage client.
@@ -63,11 +58,6 @@ func GetCloudStorageFactory() CloudStorageFactory {
 		cloudStorageFactory = &cloudStorageFactoryImpl{}
 	}
 	return cloudStorageFactory
-}
-
-// Name return bucket name.
-func (b Bucket) Name(ctx context.Context) string {
-	return fmt.Sprintf("%s_%s", appengine.AppID(ctx), b)
 }
 
 // New return cloud storage client.
@@ -87,12 +77,12 @@ func (c *cloudStorageFactoryImpl) New(ctx context.Context, ts oauth2.TokenSource
 }
 
 // DownloadFile donwload from cloud storage, then return file data.
-func (c *cloudStorageClient) DownloadFile(b Bucket, filename string) ([]byte, error) {
-	oh := c.client.Bucket(b.Name(c.ctx)).Object(filename)
-	r, err := oh.NewReader(c.ctx)
+func (c *cloudStorageClient) DownloadFile(bucket string, filename string) ([]byte, error) {
+	r, err := c.client.Bucket(bucket).Object(filename).NewReader(c.ctx)
 	if err != nil {
 		return nil, err
 	}
+	defer r.Close()
 
 	file, err := ioutil.ReadAll(r)
 	if err != nil {
@@ -102,9 +92,8 @@ func (c *cloudStorageClient) DownloadFile(b Bucket, filename string) ([]byte, er
 }
 
 // CreateFile create file in cloud storage.
-func (c *cloudStorageClient) CreateFile(b Bucket, filename string, r io.Reader) error {
-	oh := c.client.Bucket(b.Name(c.ctx)).Object(filename)
-	w := oh.NewWriter(c.ctx)
+func (c *cloudStorageClient) CreateFile(bucket string, filename string, r io.Reader) error {
+	w := c.client.Bucket(bucket).Object(filename).NewWriter(c.ctx)
 	defer w.Close()
 
 	data, err := ioutil.ReadAll(r)
@@ -120,8 +109,8 @@ func (c *cloudStorageClient) CreateFile(b Bucket, filename string, r io.Reader) 
 }
 
 // DeleteFile delete file in cloud storage.
-func (c *cloudStorageClient) DeleteFile(b Bucket, filename string) error {
-	oh := c.client.Bucket(b.Name(c.ctx)).Object(filename)
+func (c *cloudStorageClient) DeleteFile(bucket string, filename string) error {
+	oh := c.client.Bucket(bucket).Object(filename)
 	if err := oh.Delete(c.ctx); err != nil {
 		return err
 	}
@@ -129,10 +118,9 @@ func (c *cloudStorageClient) DeleteFile(b Bucket, filename string) error {
 }
 
 // Copy copy file in cloud storage from src to dst.
-func (c *cloudStorageClient) Copy(b Bucket, src, dst string) error {
-	bh := c.client.Bucket(b.Name(c.ctx))
-	soh := bh.Object(src)
-	doh := bh.Object(dst)
+func (c *cloudStorageClient) Copy(srcBucket string, dstBucket string, srcFile string, dstFile string) error {
+	soh := c.client.Bucket(srcBucket).Object(srcFile)
+	doh := c.client.Bucket(dstBucket).Object(dstFile)
 	if _, err := doh.CopierFrom(soh).Run(c.ctx); err != nil {
 		return err
 	}
@@ -140,12 +128,12 @@ func (c *cloudStorageClient) Copy(b Bucket, src, dst string) error {
 }
 
 // Move move file in cloud storage from src to dst.
-func (c *cloudStorageClient) Move(b Bucket, src, dst string) error {
-	if err := c.Copy(b, src, dst); err != nil {
+func (c *cloudStorageClient) Move(srcBucket string, dstBucket string, srcFile string, dstFile string) error {
+	if err := c.Copy(srcBucket, dstBucket, srcFile, dstFile); err != nil {
 		return err
 	}
 
-	if err := c.DeleteFile(b, src); err != nil {
+	if err := c.DeleteFile(srcBucket, srcFile); err != nil {
 		return err
 	}
 
